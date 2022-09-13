64f6090c22913cf14fca43561a7b5d66e45d94ab
Martin <martinstochi0@gmail.com>
2022-09-13 13:33:15 -0700

    All commits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -> adds two integer and returns the result
+ * @i: parameter
+ * @j: parameter
+ *
+ * Return: returns the result
+ **/
+int add(int i, int j)
+{
+int x  = j + i;
+	return (x);
+}
